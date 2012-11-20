845ef2e5810e77195d4632c4e9889b0b3464dca3
Francois-Xavier Soubirou <soubirou@yahoo.fr>
2012-11-20 23:15:17 +0100

    MopaBootstrap Bundle configuration. Core Bundle. Bootstrap layout.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,9 @@ class AppKernel extends Kernel {
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Map\CoreBundle\MapCoreBundle(),
             new Map\AdminBundle\MapAdminBundle(),
         );
 
---
+++ src/Map/CoreBundle/Menu/NavbarMenuBuilder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Map\CoreBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Mopa\Bundle\BootstrapBundle\Navbar\AbstractNavbarMenuBuilder;
+
+class NavbarMenuBuilder extends AbstractNavbarMenuBuilder
+{
+    protected $securityContext;
+    protected $isLoggedIn;
+    
+    public function __construct(FactoryInterface $factory, SecurityContextInterface $securityContext)
+    {
+        parent::__construct($factory);
+    
+        $this->securityContext = $securityContext;
+        // $this->isLoggedIn = $this->securityContext->isGranted('IS_AUTHENTICATED_FULLY');
+    }
+    
+    public function createMainMenu(Request $request)
+    {
+        $menu = $this->factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav');
+    
+        $menu->addChild('Home', array('route' => 'adminApplication_add'));
+        
+        $this->addDivider($menu, true);
+                
+        $dropdownAdmin = $this->createDropdownMenuItem(
+            $menu, "Admin", false, array('icon' => 'caret')
+        );
+        $dropdownAdmin->addChild('Preferences', array('route' => 'adminApplication_add'));
+        $dropdownAdmin->addChild('Companies', array('route' => 'adminApplication_index'));
+    
+        return $menu;
+    }
+    
+    public function createRightSideDropdownMenu(Request $request)
+    {
+        $menu = $this->factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav pull-right');
+    
+        $menu->addChild('Log out', array('route' => 'adminApplication_add'));
+    
+        return $menu;
+    }
+}
